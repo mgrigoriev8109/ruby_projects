55ce206bba5b7d76e516a1d3326e5f2fa6923f98
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-19 14:46:58 -0400

    Working on capitalized array

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,30 +1,24 @@
 
 
 def caesar_cipher (string, number_shift)
-    #Code take string, turns it into numbers 1-26, removes number_shift from the number
-    # turns the numbers back into a string and puts it onto the terminal
     string_array = string.split("")
-    p string_array
-    #turns string into an array
+   
     alphabet = ('a'..'z').to_a
-    #creates alphabet array, and the index numbers will be used to shift the string
-    only_letters = string_array.select {|element| alphabet.include?(element) }
-    p only_letters
-    only_numbers = only_letters.map {|character| character = alphabet.index(character) }
-    p only_numbers
+    capitalized_alphabet = ('A'..'Z').to_a
+
+    capitalized_array = string_array.filter_map {|element| element = element capitalized_alphabet.include?(element)}
+    lowercase_array = string_array.select {|element| alphabet.include?(element) }
+    only_numbers = lowercase_array.map {|character| character = alphabet.index(character) }
     modified_numbers = only_numbers.map {|number| number -= number_shift }
-    p modified_numbers
     modified_letters = modified_numbers.map {|character| character = alphabet[character] }
-    p modified_letters
     
     modified_array = []
     
     string_array.each do |element|
-        if alphabet.include?(element)
+        if #code to check if element-index_of == capitalize_array
+        else if alphabet.include?(element)
             modified_array.append(modified_letters.shift)
-            p modified_array
         else modified_array.append(element)
-            p modified_array
         end
     end
     p modified_array.join("")
