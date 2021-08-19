59459c6627e231c343b8c2f4acdbb9462abd952a
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-19 14:32:49 -0400

    Functions with whitespace not punctuation

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,33 @@
+
+
 def caesar_cipher (string, number_shift)
     #Code take string, turns it into numbers 1-26, removes number_shift from the number
     # turns the numbers back into a string and puts it onto the terminal
-    string = string.split("")
+    string_array = string.split("")
+    p string_array
     #turns string into an array
     alphabet = ('a'..'z').to_a
     #creates alphabet array, and the index numbers will be used to shift the string
-    string.map! {|character| character = alphabet.index(character) }
-    string.map! {|number| number -= number_shift}
-    string.map! {|character| character = alphabet[character]}
-
-    p string
+    only_letters = string_array.select {|element| alphabet.include?(element) }
+    p only_letters
+    only_numbers = only_letters.map {|character| character = alphabet.index(character) }
+    p only_numbers
+    modified_numbers = only_numbers.map {|number| number -= number_shift }
+    p modified_numbers
+    modified_letters = modified_numbers.map {|character| character = alphabet[character] }
+    p modified_letters
+    
+    modified_array = []
+    
+    string_array.each do |element|
+        if alphabet.include?(element)
+            modified_array.append(modified_letters.shift)
+            p modified_array
+        else modified_array.append(element)
+            p modified_array
+        end
+    end
+    p modified_array.join("")
 end
 
-caesar_cipher("what", 2)
+caesar_cipher("What is this!", 2)
