8dcee7d02be5c0e153d571519e04ae7ce9b400a8
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-18 23:46:33 -0400

    String array becomes numbers

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,13 +1,14 @@
 def caesar_cipher (string, number_shift)
     #Code take string, turns it into numbers 1-26, removes number_shift from the number
     # turns the numbers back into a string and puts it onto the terminal
-    string.split("")
-    string.turn_into_numbers
-    string.subtract_number_shift
-    #if number becomes 0 then it's equal to 26
-    string.join("")
-    #figure out how to create spaces after solving first problem of string->number->string
-    puts string
+    string = string.split("")
+    #turns string into an array
+    alphabet = ('a'..'z').to_a
+    #creates alphabet array, and the index numbers will be used to shift the string
+    string.map! {|character| character = alphabet.index(character) }
+    p alphabet
+
+    p string
 end
 
-caesar_cipher("What a string", 5)
+caesar_cipher("what", 2)
