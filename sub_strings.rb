69963ff67306e24b9d2aa4c0436e6ddae279c852
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-20 11:32:05 -0400

    Created #substrings

---
+++ sub_strings.rb
@@ -0,0 +1,13 @@
+=begin substring should return a hash listing each substring that
+#was found in the original strin, and how many times it was
+#found
+=end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word, dictionary)
+  #take word and turn it into array
+  word.to_a
+  #create an empty hash which will contain the substrings found in og string
+
+  #cycle through dictionary using each, if 
+end
