968315bbb00448d4203960ce19f35cfb7898365a
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-19 23:38:49 -0400

    Cleaned code

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,3 @@
-
 def string_to_hash(string)
     string_array = string.split("")
     index_array = Hash[ string_array.map.with_index {|char, index| [index, char]}]
@@ -12,15 +11,15 @@ def array_numbers_to_chars(hash, alphabet)
     hash.map {|index, char| [index, char = alphabet[char]]}
 end
 
-def seperate_hashes(hash, number_shift)
+def caesar_cipher(string, number_shift)
     lowercase_alphabet = ('a'..'z').to_a
     uppercase_alphabet = ('A'..'Z').to_a
     total_alphabet = lowercase_alphabet + uppercase_alphabet
+    input_hash = string_to_hash(string)
 
-    uppercase_hash = hash.select {|index, char| uppercase_alphabet.include?(char)}
- 
-    lowercase_hash = hash.select {|index, char| lowercase_alphabet.include?(char)}
-    other_character_hash = hash.delete_if {|index, char| total_alphabet.include?(char)}
+    uppercase_hash = input_hash.select {|index, char| uppercase_alphabet.include?(char)}
+    lowercase_hash = input_hash.select {|index, char| lowercase_alphabet.include?(char)}
+    other_character_hash = input_hash.delete_if {|index, char| total_alphabet.include?(char)}
 
     uppercase_numbers_array = hash_values_to_numbers(uppercase_hash, uppercase_alphabet, number_shift)
     lowercase_numbers_array = hash_values_to_numbers(lowercase_hash, lowercase_alphabet, number_shift)
@@ -30,33 +29,6 @@ def seperate_hashes(hash, number_shift)
 
     sorted_combined_hashes = Hash[uppercase_modified_hash.merge(lowercase_modified_hash).merge(other_character_hash).sort]
     p sorted_combined_hashes.values.to_a.join("")
-
 end
 
-seperate_hashes(string_to_hash("WhAt IsthIIs!!. "), 5)
-=begin
-def caesar_cipher (string, number_shift)
-
-    lowercase_alphabet = ('a'..'z').to_a
-    capitalized_alphabet = ('A'..'Z').to_a
-
-    capitalized_array = string_array.filter_map {|element| element = element capitalized_alphabet.include?(element)}
-    lowercase_array = string_array.select {|element| alphabet.include?(element) }
-    only_numbers = lowercase_array.map {|character| character = alphabet.index(character) }
-    modified_numbers = only_numbers.map {|number| number -= number_shift }
-    modified_letters = modified_numbers.map {|character| character = alphabet[character] }
-    
-    modified_array = []
-    
-    string_array.each do |element|
-        if #code to check if element-index_of == capitalize_array
-        else if alphabet.include?(element)
-            modified_array.append(modified_letters.shift)
-        else modified_array.append(element)
-        end
-    end
-    p modified_array.join("")
-end
-
-caesar_cipher("What is this!", 2)
-=end
+caesar_cipher("WhAt IsthIIs!!. ", 5)
