46786137cab66d1043ee2cc61ff43cb3d07e432f
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-22 16:15:52 -0400

    Clean up code

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,7 +1,3 @@
-=begin substring should return a hash listing each substring that
-#was found in the original strin, and how many times it was
-#found
-=end
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(input_word, dictionary)
@@ -9,18 +5,15 @@ def substrings(input_word, dictionary)
 
   matching_words = dictionary.reduce (Hash.new(0)) do |result_hash, dictionary_word|
     input_word_array.each do |word|
-
       if word.include?(dictionary_word)
         result_hash[dictionary_word]+= 1
       end
-
     end
 
     result_hash
   end
-    
-    p matching_words
 
+  p matching_words
 end
 
 substrings("below howdy", dictionary)
