beb6c7993211612f0c8d5764c39d7fd2db3fbc63
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-22 16:21:40 -0400

    Final code stylistic changes

--- sub_strings.rb
+++ sub_strings.rb
@@ -5,8 +5,8 @@ def substrings(input_word, dictionary)
 
   matching_words = dictionary.reduce (Hash.new(0)) do |result_hash, dictionary_word|
     input_word_array.each do |word|
-      if word.include?(dictionary_word)
-        result_hash[dictionary_word]+= 1
+      if word.downcase.include?(dictionary_word)
+        result_hash[dictionary_word] += 1
       end
     end
 
@@ -16,4 +16,4 @@ def substrings(input_word, dictionary)
   p matching_words
 end
 
-substrings("below howdy", dictionary)
+substrings("Below below howdy it!", dictionary)
