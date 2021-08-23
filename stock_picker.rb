dec571deb971d7fc03aa09ff457880cffac1a9c5
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-23 00:48:27 -0400

    syntax error need to use pry

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,4 +1,6 @@
-def stock_picker
+require 'pry-byebug'
+
+def stock_picker(array_of_days)
   #reduce would probably be best for this
   #it starts at the first value as the accumulator [0, 0, 0] for [buy, sell, possible_buy]
 
@@ -15,6 +17,26 @@ def stock_picker
   #if array comes to a new possible lowest buy value, it sets that to possible_buy to test out in the future
 
   #only problem is that this current code will return values, not indexes, but that seems like a simple problem to solve
+  binding.pry
+  highest_profit = array_of_days.reduce(Array.new(3)) do |profit_array, current_value|
+    if (current_value<profit_array[0] && profit_array[0]==profit_array[2])
+        profit_array[0]= current_value
+        profit_array[1]= current_value
+        profit_array[2]= current_value
+    elsif ((current_value-profit_array[2]) > (profit_array[1]-profit_array[0]))
+        profit_array[0]=profit_array[2]
+        profit_array[1]=current_value
+    elsif current_value>profit_array[1]
+        profit_array[1]=current_value
+    else current_value<profit_array[0]
+        profit_array[2]=current_value
+    end
+    profit_array
+    
+  end
+  p highest_profit
 end
 
-> stock_picker([17,3,6,9,15,8,6,1,10]
+> stock_picker([17,3,6,9,15,8,6,1,10])
+
+    
