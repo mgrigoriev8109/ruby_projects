675eb967ebe14dd6b077be800c15c73fb45a330e
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-25 11:32:10 -0400

    Begin until loop 's reduce

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,15 +2,14 @@ def bubble_sort(array)
 swapped = true
   until swapped == false
     swapped = false
-    array.map!.with_index do|number, index|
-        if number > array[number+1]
-            array[index], array[index+1] = array[index+1], array[index]
-            swapped = true
-        else next
-        end
+    array.reduce(Array.new(0)) do|accumulator_array, current_number|
+      #if a swap occurs, swapped = true
+      
+      accumulator_array
+        
     end
-  p array
   end
+  p array
 end
 
 bubble_sort([4,3,78,2,0,2])
