eb791f22f8b2b9370cf0cee76477b217b19367ad
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-25 22:11:42 -0400

    Start over, try different problem solving approach

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,5 @@
 def bubble_sort(array)
-swapped = true
-  until swapped == false
-    swapped = false
-    array.reduce(Array.new(0)) do|accumulator_array, current_number|
-      #if a swap occurs, swapped = true
-      
-      accumulator_array
-        
-    end
-  end
-  p array
+
 end
 
 bubble_sort([4,3,78,2,0,2])
